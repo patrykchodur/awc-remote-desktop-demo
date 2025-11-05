bf7858d6bb34983c4a786434fa240c7abe77f26d
pchodur <pchodur@google.com>
2025-11-05 11:24:58 +0000

    Add awc features

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
+/* helper for testing */ const sleep = (ms: number) => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+(window as any).sleep = sleep;
+
+const getDisplayState = () => {
+  if (window.matchMedia('(display-state: maximized)').matches) {
+    return 'maximized';
+  } else if (window.matchMedia('(display-state: minimized)').matches) {
+    return 'minimized';
+  } else if (window.matchMedia('(display-state: fullscreen)').matches) {
+    return 'fullscreen';
+  } else if (window.matchMedia('(display-state: normal)').matches) {
+    return 'normal';
+  }
+}
 
 const countElement = document.querySelector<HTMLSpanElement>('#count')!;
 
@@ -16,3 +32,36 @@ document.querySelector<HTMLButtonElement>('#decrement')!.addEventListener('click
 document.querySelector<HTMLButtonElement>('#reset')!.addEventListener('click', () => {
   updateCount(0);
 });
+
+document.querySelector<HTMLButtonElement>('#minimize')!.addEventListener('click', async () => {
+  console.log(`before minimize: diplay-state = ${getDisplayState()}, inner height = ${window.innerHeight}, inner width = ${window.innerWidth}`);
+  await(window as any).minimize();
+  console.log(`after minimize: diplay-state = ${getDisplayState()}, inner height = ${window.innerHeight}, inner width = ${window.innerWidth}`);
+});
+
+document.querySelector<HTMLButtonElement>('#maximize')!.addEventListener('click', async () => {
+  console.log(`before maximize: diplay-state = ${getDisplayState()}, inner height = ${window.innerHeight}, inner width = ${window.innerWidth}`);
+  await (window as any).maximize();
+  console.log(`after maximize: diplay-state = ${getDisplayState()}, inner height = ${window.innerHeight}, inner width = ${window.innerWidth}`);
+});
+
+document.querySelector<HTMLButtonElement>('#restore')!.addEventListener('click', async () => {
+  console.log(`before restore: diplay-state = ${getDisplayState()}, inner height = ${window.innerHeight}, inner width = ${window.innerWidth}`);
+  await (window as any).restore();
+  console.log(`after restore: diplay-state = ${getDisplayState()}, inner height = ${window.innerHeight}, inner width = ${window.innerWidth}`);
+});
+
+document.querySelector<HTMLButtonElement>('#moveToCorner')!.addEventListener('click', () => {
+  (window as any).moveTo(3000, 3000);
+});
+
+const resizableElement = document.querySelector<HTMLInputElement>('#resizable')!;
+resizableElement.addEventListener('change', () => {
+  (window as any).setResizable(resizableElement.checked);
+})
+
+const movedTimeElement = document.querySelector<HTMLSpanElement>('#movedTime')!;
+const updateTimeMoved = () => {
+  movedTimeElement.textContent = Date().toLocaleString();
+}
+(window as any).onmove = () => updateTimeMoved();
